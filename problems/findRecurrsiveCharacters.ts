1e513a0599b48e4d235a6e6fd272d5b982a2db55
Waleed Ahmed <>
2022-12-09 18:25:01 +0500

    done

---
+++ problems/findRecurrsiveCharacters.ts
@@ -0,0 +1,15 @@
+type resultType = string;
+
+const repeatedCharacter = (input: string) => {
+    let result: resultType[] = []
+      for (let i = 0; i < input.length - 1; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+          if (input[i] === input[j]) {
+            result.push(input[i]) ;
+          }
+        }
+      }
+      return result;
+    };
+    console.log(repeatedCharacter("waleed"));
+    
