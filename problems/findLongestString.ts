4e2cea5787036bfd701ddca259ee431baa793c25
Waleed Ahmed <>
2022-12-28 16:55:41 +0500

    done

--- problems/findLongestString.ts
+++ problems/findLongestString.ts
@@ -1,25 +1,29 @@
 
-interface IObjectKeys {
-    [key: string]: number | undefined;
-}
-const findLongestString = (input: string[]) => {
-    if (input.length === 0) {
-        return false;
-    }
-    let longestStrLen: number = input[0].length;
-    let longestStr = input[0]
-    let result: IObjectKeys = {}
 
-    for (let i = 1; i < input.length; i++) {
-        let tempMax = input[i].length;
-        if (tempMax > longestStrLen) {
+interface IObjectKeys {
+    [key: string]: number | undefined
+}
+
+const findLongestString = (input: string[]) => {
+    if(input.length === 0){
+        return false
+    }
+
+    let longestStrLen: number = input[0].length
+    let longestStr = input[0]
+    let result:IObjectKeys = {}
+
+    for(let i=1; i< input.length; i++){
+        let tempMax = input[i].length
+        if(tempMax > longestStrLen){
             longestStr = input[i]
-            longestStrLen = tempMax;
+            longestStrLen = input[i].length
         }
     }
+
     result[longestStr] = longestStrLen
 
     return result;
 }
-console.log(findLongestString(["ab", "a", "abcd"]));
-console.log(findLongestString(["abc", "abc", "ab"]));
+
+console.log(findLongestString(["abc", "ac", "abbccdd"]));
